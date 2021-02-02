1dd24234ef06c1eee93ff43b93a072b140e76afa
Nahum Maciel <nahumamaciel@gmail.com>
2021-02-02 04:19:54 -0800

    Completed champter 3.

---
+++ car.rb
@@ -0,0 +1,50 @@
+class Vehicle 
+    attr_accessor :odometer
+    attr_accessor :gas_used 
+
+    def mileage
+        @odometer / @gas_used
+    end
+    
+    def accelerate
+        puts "Floor it!"
+    end
+
+    def sound_horn
+        puts "Beep! Beep!"
+    end
+
+    def steer
+        puts "Turn front 2 wheels."
+    end
+end    
+
+class Car < Vehicle
+end
+
+class Truck < Vehicle
+    attr_accessor :cargo
+
+    def load_bed(contents)
+        puts "Securing #{contents} in the truck bed."
+        @cargo = contents
+    end
+end
+
+class Motorcycle < Vehicle
+    def steer
+        puts "Turn front wheel."
+    end
+end
+
+truck = Truck.new
+truck.load_bed("259 bouncy balls")
+puts "The truck is carrying #{truck.cargo}."
+
+truck.odometer = 11432
+truck.gas_used = 366
+print "Average MPG: "
+puts truck.mileage
+
+motorcycle = Motorcycle.new
+motorcycle.steer
--- dog.rb
+++ dog.rb
@@ -1,5 +1,5 @@
-class Dog
-    attr_reader :name, :age
+class Animal
+attr_reader :name, :age
 
     def name=(value)
         if value == ""
@@ -14,19 +14,44 @@ class Dog
             raise "An age value pf #{value} is not valid!"
         else
             @age = value
-        end
     end
+end
 
-    def report_age
+def report_age
+    puts "#{@name} is #{@age} years old."
+end
+
+def talk
+    puts "#{@name} says Bark!"
+end
+
+def move(destination)
+    puts "#{@name} runs to the #{destination}."
+end
+end    
+
+class Dog < Animal    
+    def to_s
         puts "#{@name} is #{@age} years old."
-    end
+    end 
+end
 
+class Bird < Animal    
     def talk
-        puts "#{@name} says Bark!"
-    end
+        puts "#{@name} says Chirp! Chirp!" 
+    end 
+end
 
+class Cat < Animal   
+    def talk
+        puts "#{@name} says Meow!" 
+    end 
+end
+
+class Armadillo < Animal   
     def move(destination)
-        puts "#{@name} runs to the #{destination}."
+        puts "#{@name} unrolls!"
+        super(destination)
     end
 end
 
@@ -35,12 +60,26 @@ fido.name = "Fido"
 fido.age = 2
 fido.report_age
 
-rex = Dog.new
-rex.name = "Rex"
-rex.age = 3
-rex.report_age
+# puts fido
 
-bad_dog = Dog.new
-bad_dog.name = ""
-bad_dog.age = 2
-bad_dog.report_age
+# rex = Dog.new
+# rex.name = "Rex"
+# rex.age = 3
+# rex.report_age
+
+# bad_dog = Dog.new
+# bad_dog.name = ""
+# bad_dog.age = 2
+# bad_dog.report_age
+
+# whiskers = Cat.new
+# whiskers.name = "whiskers"
+# whiskers.talk
+
+# polly = Bird.new
+# polly.name = "polly"
+# polly.talk
+
+# sebastian = Armadillo.new
+# sebastian.name = "sebastian"
+# sebastian.talk
